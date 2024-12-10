b05a9d651c27d5ba3af3a644ef764283de35393f
amir-gorgy <amir.gorgy.98@gmail.com>
2024-12-09 20:05:25 -0800

    Added advent_1 parts one and two

--- Days_py/Day_1/advent_1.py
+++ Days_py/Day_1/advent_1.py
@@ -1,9 +1,12 @@
 import sys
-import ast
+import pandas as pd
 
-def total_distance(s,t):
-    s_sorted = sorted(s)
-    t_sorted = sorted(t)
+def total_distance(puzzle_input):
+    
+    data = pd.read_table(puzzle_input, header = None, sep = '   ')
+    
+    s = sorted(data[:][0])
+    t = sorted(data[:][1])
     
     l_s = len(s)
     l_t = len(t)
@@ -11,19 +14,38 @@ def total_distance(s,t):
     res = []
     
     for i in range(min(l_s, l_t)):
-        res.append(abs(s_sorted[i]-t_sorted[i]))
+        res.append(abs(s[i]-t[i]))
     
     if l_s > l_t:
         for i in range(l_t,l_s):
-            res.append(s_sorted[i])
+            res.append(s[i])
     
-    elif l_t > l_s:
+    elif l_s < l_t:
         for i in range(l_s,l_t):
-            res.append(t_sorted[i])
+            res.append(t[i])
     
     return sum(res)
 
+def similarity_score(puzzle_input):
+    data = pd.read_table(puzzle_input, header = None, sep = '   ')
+    
+    s = sorted(data[:][0])
+    t = sorted(data[:][1])
+    
+    look_up = {}
+    for num in t:
+        if num in look_up.keys():
+            look_up[num] += 1
+        else:
+            look_up[num] = 1
+
+    res = 0
+    for num in s:
+        if num in look_up.keys():
+            res = res + num * look_up[num]
+    return res
+
 if __name__ == "__main__":
-    s = ast.literal_eval(sys.argv[1])
-    t = ast.literal_eval(sys.argv[2])
-    print(total_distance(s,t))
+    puzzle_input = sys.argv[1]
+    print(total_distance(puzzle_input))
+    print(similarity_score(puzzle_input))
